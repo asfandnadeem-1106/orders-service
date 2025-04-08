8d5c1080484e60fd119f69fdb186d7186fffacef
asfandnadeem-1106 <asfandnadeem116@gmail.com>
2025-04-08 22:05:36 +0500

    removed swagger

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,21 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
-  const options = new DocumentBuilder()
-    .addBearerAuth()
-    .setTitle('Cities APP')
-    .setDescription('Cities API documentation')
-    .setVersion('1.0')
-    .addTag('Cities')
-    .build()
-    
-  const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup('api', app, document)
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
